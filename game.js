c8ebd7b0e795ca3a131b5f665a7269409c745922
josephdantos01-gif <josephdantos01@gmail.com>
2026-08-12 21:57:15 -0600

    aa

--- game.js
+++ game.js
@@ -1,7 +1,3 @@
-// ==========================================
-// CANVAS
-// ==========================================
-
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
